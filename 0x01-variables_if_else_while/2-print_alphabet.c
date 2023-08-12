00f6db73ac2904a63969fe6df1702e2e9008c445
rukenya321 <rukenyamunenejoel@gmail.com>
2023-08-12 11:27:42 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char alPhabet = 'a';
+	
+	while (alPhabet <= 'z')
+	{
+		putchar(alPhabet);
+		alPhabet++;
+	}
+	putchar('\n');
+
+	return (0);
+}
