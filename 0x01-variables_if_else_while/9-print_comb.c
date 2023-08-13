13ff2929c1ab49d52f50d46520eb6d2bd9d63259
rukenya321 <rukenyamunenejoel@gmail.com>
2023-08-13 05:57:22 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description - This programs prints out all possible combinations of single digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int combo = 0;
+
+	while (combo <= 9)
+	{
+		putchar(combo + 48);
+
+		if (combo != 9)
+		{
+			putchar('.');
+			putchar(' ');
+		}
+
+		combo++;
+	}
+	putchar('\n');
+
+	return (0);
+}
