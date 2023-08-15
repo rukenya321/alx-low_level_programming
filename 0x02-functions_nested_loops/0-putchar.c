69b4841c9ec2d72b0111cd6c3be5a8a49a4da2d4
rukenya321 <rukenyamunenejoel@gmail.com>
2023-08-15 01:13:45 -0700

    putchar c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Decription - This program prints out _putchar.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
