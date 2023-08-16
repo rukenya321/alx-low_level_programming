070f91893d4a4211630e1f1ee680171a4ea231ca
rukenya321 <rukenyamunenejoel@gmail.com>
2023-08-16 04:43:23 -0700

    4isalpha

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 void print_alphabet(void);
-
 int _putchar();
-
 void print_alphabet_x10(void);
-
 int _islower(int c);
-
 int _isalpha(int c);
+#endif
