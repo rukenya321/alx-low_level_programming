a3d4dafd9f0892965bcb0e9db794a5afe4bf8dd5
rukenya321 <rukenyamunenejoel@gmail.com>
2023-08-18 04:57:14 -0700

    10add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * @u: Integer 1
+ *
+ * @i: Integer 2
+ *
+ * Return: Always 0.
+ */
+
+int add(int u, int i)
+{
+	int result = u + i;
+
+	printf("%d\n", result);
+
+	return (0);
+}
