9cd78f75f7f913531902476041f87e4c24bbd818
rukenya321 <rukenyamunenejoel@gmail.com>
2023-08-18 03:13:29 -0700

    9timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,11 +10,11 @@ void times_table(void)
 {
 	int h, v;
 
-	for (h = 0; h <= 9; h++)
+	for (h = 0; h <= 9; ++h)
 	{
-		for (v = 0; v <= 9; v++)
+		for (v = 0; v <= 9; ++v)
 		{
-			printf("%d\t", h * v);
+			printf("%2d, ", h * v);
 		}
 
 		printf("\n");
