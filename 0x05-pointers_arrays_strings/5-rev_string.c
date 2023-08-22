ef1d5fc486f7c3977b6c7e1ceb264ef97e65e0aa
rukenya321 <rukenyamunenejoel@gmail.com>
2023-08-22 02:15:35 -0700

    5rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,12 +12,14 @@
 void rev_string(char *s)
 {
 	int len = strlen(s);
+
 	int begin = 0;
 	int stop = len - 1;
 
 	while (begin < stop)
 	{
 		char test = s[begin];
+
 		s[begin] = s[stop];
 		s[stop] = test;
 
