9969c0726cda1f14918884b3972c094d7e982521
rukenya321 <rukenyamunenejoel@gmail.com>
2023-08-28 02:48:45 -0700

    print not 24

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * @two4: Int for the test
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int two4;
+
+	for (two4 = 0; two4 <= 9; two4++)
+	{
+		if (two4 == 2 || two4 == 4)
+			continue;
+		_putchar(two4 + 48);
+	}
+	_putchar('\n');
+}
+
