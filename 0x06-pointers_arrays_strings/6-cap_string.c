34406fc9691058e11c31b68e2068259ad2efb073
rukenya321 <rukenyamunenejoel@gmail.com>
2023-09-04 10:24:09 +0300

    Capitalize all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,8 +4,6 @@
 #include <stdio.h>
 
 /**
- * *cap_string - Entry point
- *
  * separators - They separate words.
  *
  * @s: Int for the test.
@@ -13,31 +11,37 @@
  * Return: Nothing.
  */
 
-bool separators(char s) /* separators - They separate words */
+bool separators(char s)
 {
 	char separator_members[] = " \t\n,;.!?\"(){}";
 	int a;
 
-	for (a = 0; separator_member[a] != '\0'; a++)
+	for (a = 0; separator_members[a] != '\0'; a++)
 	{
-		if (s == separator_member[i])
+		if (s == separator_members[a])
 		{
 			return (true);
 		}
 	}
 	return (false);
 }
+/**
+ * *cap_string - This capitalizes all words.
+ *
+ * @str: First character
+ *
+ * Return: str
+ */
 
-char *cap_string(char *) /* *cap_string - This capitalizes all words */
+char *cap_string(char *str)
 {
 	bool new_string = true;
 
-	char *str;
 	int a;
 
 	for (a = 0; str[a] != '\0'; a++)
 	{
-		if (separatror(str[a]))
+		if (separators(str[a]))
 		{
 			new_string = true;
 		}
