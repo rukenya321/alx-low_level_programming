28bb16b5942c21f88886cc9a213ace7479dd1109
rukenya321 <rukenyamunenejoel@gmail.com>
2023-09-06 13:06:47 +0300

    new changes

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - Pointer to char.
+ *
+ * @s: Double pointer.
+ *
+ * @to: Pointer to char.
+ *
+ * Return: nothing.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
